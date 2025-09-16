b5af0f449c41a5e19ecb6a6389a6caf022142f7c
Kacper <07Potion@gmail.com>
2025-09-16 18:04:13 +0200

    Add logout button on all sites

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,38 @@
+"use client"
 import './globals.css'
+import { authClient } from '@/lib/auth/auth-client'
 
 import { Toaster } from "@/components/ui/sonner"
+
+import { Logout } from '@/components/logout'
+import { useState, useEffect } from 'react'
+
+
+
+
 
 export default function IndexLayout({
     children,
 }: {
     children: React.ReactNode
 }) {
+      const [session, setSession] = useState<any>(null)
+        useEffect(() => {
+      const fetchSession = async () => {
+        const { data: sessionData } = await authClient.getSession()
+        setSession(sessionData)
+      }
+      fetchSession()
+    }, [])
     return (
         <html lang="en">
             <body>
+                { session ? (
+                    <div className="absolute top-4 right-4">
+                            <Logout />
+                            </div>
+                ) : (<></>)
+                }
 
                 <main className='min-h-screen w-screen bg-slate-300'>{children}</main>
                 <Toaster />
